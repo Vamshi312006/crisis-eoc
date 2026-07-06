2d40257030e89ae6da16202217ee855afd3c2947
Vamshi Krishna <kv312006@gmail.com>
2026-07-06 23:10:09 +0530

    Prepare for Render deployment

--- app.py
+++ app.py
@@ -129,5 +129,8 @@ def update(id):
     return jsonify(success=False)
 
 
-if __name__=="__main__":
-    app.run(debug=True)
+import os
+
+if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
